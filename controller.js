a6f02de6460c5a33921a6b30d084e97ab089c960
John Jordan <johnjordan1985@gmail.com>
2022-11-14 15:57:29 -0800

    refactor: views and logic in controller

--- controller.js
+++ controller.js
@@ -1,53 +1,37 @@
 try {
-  const widgetRef = window.stateMachineFactory(window.syllogismDescription);
-
   const addHandlers = function addHandlers(widget) {
-    try {
-      const machineDomTarget = document.getElementById("machine-target");
+    const machineDomTarget = document.getElementById("machine-target");
 
-      const renderMachineToDOM = function (machine, DOMTarget) {
-        // eslint-disable-next-line no-param-reassign
-        DOMTarget.innerHTML = (window.machineDOMTemplate(machine));
-      };
+    const renderMachineToDOM = function (machineState, DOMTarget) {
+      // eslint-disable-next-line no-param-reassign
+      DOMTarget.innerHTML = (window.machineDOMTemplate(machineState));
+    };
 
-      renderMachineToDOM(widget, machineDomTarget);
+    const processEventToStateMachine = function (event, machine) {
+      const initialState = widget.state;
 
-      const processEventToStateMachine = function (event, machine) {
-        const initialState = widget.statePath;
+      machine.sendEvent(event);
 
-        machine.sendEvent(event);
+      if (initialState !== machine.state) {
+        console.log("state has changed!");
+        renderMachineToDOM(machine.state, machineDomTarget);
+      }
+    };
 
-        if (initialState !== machine.statePath) {
-          renderMachineToDOM(machine, machineDomTarget);
-        }
-      };
+    renderMachineToDOM(widget.state, machineDomTarget);
 
-      // setTimeout(() => {
-      //   // processEventToStateMachine("ERROR", window.widget);
-      //   processEventToStateMachine("LOAD", widget);
-      // }, 2000);
-
-      window.widget = widget;
-
-      document.addEventListener("click", (e) => {
-        const elem = e.target;
-        const eventType = elem.dataset && elem.dataset.event;
-        if (eventType) {
-          if (eventType === "RELOAD") {
-            window.widget = window.stateMachineFactory(window.syllogismDescription);
-            renderMachineToDOM(window.widget, machineDomTarget);
-            return;
-          }
-          processEventToStateMachine(eventType, widget);
-        }
-      });
-    } catch (e) {
-      console.error("Catching error: ", e);
-      widget.sendEvent("ERROR");
-    }
+    document.addEventListener("click", (e) => {
+      e.preventDefault();
+      const elem = e.target;
+      const eventType = elem.dataset && elem.dataset.event;
+      if (eventType) {
+        processEventToStateMachine(eventType, widget);
+      }
+    });
+    window.widget = widget;
   };
 
-  addHandlers(widgetRef);
+  addHandlers(window.stateMachineFactory(window.syllogismDescription));
 } catch (error) {
   // eslint-disable-next-line no-console
   console.error("An error occured: ", error);
--- views.js
+++ views.js
@@ -1,31 +1,31 @@
-const getActionButton = function (statePath) {
-  const types = {
-    error: {
-      msg: "Reload",
-      event: "RELOAD"
-    },
-    "loaded.clean": {
-      msg: "Touch",
-      event: "TOUCH"
-    },
-    "loaded.dirty": {
-      msg: "Clean",
-      event: "CLEAN"
-    }
-  };
-  if (!types[statePath]) return "";
-  const type = types[statePath];
-  const button = `<button data-event="${type.event}">${type.msg}</button>`;
-  return button;
+const getDirectFigureNav = function () {
+  return `<nav>
+  <a href="#" data-event="SWITCH-FIRST">I</a>
+  <a href="#" data-event="SWITCH-SECOND">II</a>
+  <a href="#" data-event="SWITCH-THIRD">III</a>
+  <a href="#" data-event="SWITCH-FOURTH">IV</a></nav>`;
+};
+const getSwitchButton = function (termLabel) {
+  return `<button data-event="SWITCH-${termLabel.toUpperCase()}">Switch</button>`;
 };
 
-const machineDOMTemplate = (machine) => {
-  const { statePath } = machine;
+const getTerm = function (premiseState) {
+  console.log("premiseState is ", premiseState);
+  const subject = premiseState.states.subject.label;
+  const predicate = premiseState.states.predicate.label;
+  return `${subject} -- ${predicate}`;
+};
+
+const machineDOMTemplate = (machineState) => {
+  const { label } = machineState;
+  console.log(machineState.states.majorPremise);
 
   const response = `<section>
-        <p>Machine is in the "${machine.getStateFrameFromPath().label}" state</p>
+      ${getDirectFigureNav()}
+        <p>Machine is in the "${label}" figure</p>
       </section>
-      ${getActionButton(statePath)}`;
+      <section id="majorTerm">${getTerm(machineState.states.majorPremise)} ${getSwitchButton("major")}</section>
+      <section id="minorTerm">${getTerm(machineState.states.minorPremise)} ${getSwitchButton("minor")}</section>`;
 
   return response;
 };
