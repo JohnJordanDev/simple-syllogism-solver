97001f85c4a7640e8d2d2c4dc8237f9514a395e7
John Jordan <johnjordan1985@gmail.com>
2022-11-14 17:00:44 -0800

    refactor: remove most pre-existing logic and tailor code for specific syllogism

--- state-machine-library.js
+++ state-machine-library.js
@@ -3,42 +3,78 @@
  * Notes
  * 1. Difference between 'jump' (using statePath; doesn't trigger entry/exit actions)
  *  and 'transition' (transition state; does trigger entry/exit actions)
- * 2. When adding a new state (atomic or nested), need to add to validState list
  */
 try {
   window.stateMachineFactory = (function () {
-    const getInitialState = function (machineDescription, historyStates) {
-      if (machineDescription.type === "parallel") {
-        // TODO: do things here...
-        return;
-      }
-      if (!(machineDescription.initial && machineDescription.states)) return;
+    const getID = (function () {
+      let i = 0;
+      return function () {
+        const id = i;
+        i += 1;
+        return id;
+      };
+    })();
+    const disallowedKeyNames = ["id", "initial", "label", "on", "onEntry", "onExit", "type"];
+    const depthFirstLabelling = function (o) {
+      const keys = Object.keys(o);
+      let keyName;
+      let keyValue;
+      for (let i = 0; i < keys.length; i += 1) {
+        keyName = keys[i];
+        keyValue = o[keyName];
 
-      let availableStates = machineDescription.states;
-      let initialState = availableStates[machineDescription.initial];
-      console.log(availableStates);
-      historyStates.push(machineDescription);
-      historyStates.push(initialState);
-
-      while (initialState.initial && initialState.states && initialState.states[initialState.initial]) {
-        availableStates = initialState.states;
-        console.log(availableStates);
-        initialState = availableStates[initialState.initial];
-        historyStates.push(initialState);
-
-        // console.log
-        if (initialState.type && initialState.type === "parallel") {
-          // do something here
-          return;
+        // TODO Add list of excluded property names, e.g. ID, initial, type etc.
+        if (disallowedKeyNames.indexOf(keyName) === -1) {
+          if (keyName !== "states" && !keyValue.label) {
+            keyValue.label = keyName;
+            keyValue.id = getID();
+          }
+          depthFirstLabelling(keyValue);
         }
       }
+      // eslint-disable-next-line consistent-return
+    };
+    /**
+     *
+     * @param {Object} machineDescription total description of all machine states
+     * @returns {Object} initialState inital state of the machine
+     */
+    const getInitialState = function (machineDescription) {
+      let initialState = {};
+
+      const availableFigures = machineDescription.states;
+      const initialFigure = availableFigures[machineDescription.initial];
+
+      initialState = { ...initialFigure };
+
+      let { majorPremise } = initialFigure.states;
+      majorPremise = majorPremise.states[majorPremise.initial];
+
+      let { minorPremise } = initialFigure.states;
+      minorPremise = minorPremise.states[minorPremise.initial];
+
+      initialState.states = { majorPremise, minorPremise };
       return initialState;
     };
+    /**
+     *
+     * @param {Object} machineDescription total description of machine, to be decorated
+     * @returns {Object} o2Dec decorated total description of machine
+     */
+    const getMachineDescription = function (machineDescription) {
+      const o2Dec = JSON.parse(JSON.stringify(machineDescription));
+      o2Dec.id = getID();
+      o2Dec.label = "rootState";
+      depthFirstLabelling(o2Dec);
+      return o2Dec;
+    };
     const Factory = function (stateMachineDescription) {
+      // TODO: figure out how to decorate all objects with label after key
+      const _machineDescription = getMachineDescription(stateMachineDescription);
       const _historyStates = [];
-      let _state = getInitialState(stateMachineDescription, _historyStates);
+      let _state = getInitialState(_machineDescription);
 
-      let _statePath = stateMachineDescription.initial;
+      const _statePath = stateMachineDescription.initial;
       let _pseudoStates = [];
 
       const m = {
@@ -58,244 +94,43 @@ try {
         },
         get statePath() { return _statePath; },
         set statePath(newPath) {
-          if (!this.isValidPath(newPath)) return;
-          _statePath = newPath;
-          _state = this.getStateFrameFromPath(_statePath);
-          console.info("State has changed...", [this.state, this.statePath]);
+
         },
-        ...stateMachineDescription,
-        // TODO: this could be replaced with an actual check on the state machine, to see if path corresponds to a state
-        validStates: Object.keys(stateMachineDescription["states"]).concat(stateMachineDescription["validStates"])
+        // TODO: replace this.states with this description of the machine itself.
+        get machineDescription() {
+          return _machineDescription;
+        }
       };
       return m;
     };
-    Factory.prototype.disallowedAction = function (msg) {
-      console.warn("This action is not allowed: ", msg);
-      return null;
-    };
-    /**
-         *
-         * @param {object} state state undergoing transition event
-         * @param {string} actionList name of property on state, which lists functions
-         */
-    Factory.prototype.doStateTransitionActions = function (state, actionListName = "onExit") {
-      // this.disallowedAction('action list NOT on state.');
-      if (!(state[actionListName] && Array.isArray(state[actionListName]))) return;
-      const list = state[actionListName];
-      for (let i = 0; i < list.length; i++) {
-        list[i](`'${actionListName}' actions being called on '${this.statePath}' state.`);
-      }
-    };
 
-    /**
-         * @param {string} event name of event sent to state machine
-         */
-    Factory.prototype.getPathToAncestorStateWithAction = function (event) {
-      if (!event) return console.warn('"Event" cannot be empty, when finding ancestor with action!');
-      const pathList = this.statePath.split(".");
-      pathList.pop(); // get parent path
-      let ancestorState = this.getStateFrameFromPath(pathList.join("."));
-      while (ancestorState.label !== "rootState" && pathList.length) {
-        const validActionsOnParent = ancestorState.on;
-        if (validActionsOnParent && validActionsOnParent[event]) {
-          return pathList.join(".");
-        }
-        pathList.pop();
-        ancestorState = this.getStateFrameFromPath(pathList.join("."));
-      }
-      return pathList.join(".");
-    };
-
-    Factory.prototype.getChildPathsRelativeToAncestor = function (ancestorLabel = "") {
-      const curr = this.statePath.split(".");
-      const ancesList = ancestorLabel.split(".");
-      return curr.filter((e, i) => i >= ancesList.length);
-    };
-
-    Factory.prototype.getDeepestStateLabel = function () {
-      return this.statePath.split(".").pop();
-    };
-    Factory.prototype.getParentOfCurrentGlobalState = function () {
-      const listOfNestedStates = this.statePath.split(".");
-      if (listOfNestedStates.length === 1) return this; // returning state machine itself
-      listOfNestedStates.pop(); // get parent state path
-      return this.getStateFrameFromPath(listOfNestedStates.join("."));
-    };
-    /**
-     * @param {string} path - state path to retrieve state frame from
-     * @returns {Object|Object[]} stateFrame
-     */
-    Factory.prototype.getStateFrameFromPath = function (path = this.statePath) {
-      // assume at root state, return state machine itself
-      if (!path) return this;
-
-      // first parallel state remains as global state frame
-      if (this.statePath.indexOf("-") > -1) return this.state;
-
-      const statePathList = path.split(".");
-
-      // first child state of state machine, itself
-      let currentStateFrame = this.states[statePathList[0]];
-      if (!currentStateFrame) {
-        return console.warn("Given path does not match child states of state machine (itself)");
-      }
-
-      if (statePathList.length === 1) return currentStateFrame;
-
-      for (let i = 1; i <= statePathList.length - 1; i++) {
-        if (!currentStateFrame.states) {
-          return console.warn("Tried to find nested states in atomic state");
-        }
-        if (currentStateFrame.states && currentStateFrame.states[statePathList[i]]) {
-          currentStateFrame = currentStateFrame.states[statePathList[i]];
-        } else {
-          return console.warn("Nested state does not exist on this compound state");
-        }
-      }
-      return currentStateFrame;
-    };
-    Factory.prototype.getValidChildStates = function () {
-      const p = `${this.statePath}.`;
-      return this.validStates.filter((s) => s.indexOf(p) >= 0);
-    };
-    Factory.prototype.isFirstStateInPath = function (states = [], parPath = this.getValidChildStates()) {
-      const firstState = states[0];
-      for (let i = 0; i < parPath.length; i++) {
-        if (parPath[i].indexOf(firstState) > -1) {
-          return true;
-        }
-      }
-      return false;
-    };
-    Factory.prototype.isValidPath = function (newPath) {
-      const pathExists = this.validStates.filter((path) => path === newPath).length === 1;
-      if (pathExists) return pathExists;
-      console.error("newpath is NOT on list: ", newPath);
-      throw new Error("new state path not on validState list: ");
-    };
     Factory.prototype.sendEvent = function (event) {
-      console.warn("Event: ", event);
-      const currentState = this.state;
-      if (currentState.type && currentState.type === "final") return;
-
-      const validActions = currentState.on;
-      if (!validActions) return console.warn('Present state has no actions "on" property');
-
-      const actionBeingTaken = validActions[event];
-      const targetStateLabel = actionBeingTaken && actionBeingTaken.target;
-      if (targetStateLabel) {
-        this.transitionToNewSiblingState(targetStateLabel);
-      }
-      // reverse up ancestor list to find a state that can respond to Event (if exists)
-      if (!actionBeingTaken) {
-        const pathToAncestorWithAction = this.getPathToAncestorStateWithAction(event);
-        if (pathToAncestorWithAction) {
-          // Note: difference here between 'jump' and 'transition' (entry/exit actions triggered) to states
-          // TODO: refactor this to use 'updateStatePath', and indicate a parent
-          console.warn("directly jumping ", pathToAncestorWithAction);
-          this.updateStatePath(pathToAncestorWithAction, { directToAncestor: true });
-
-          const validActions = this.state.on;
-          const actionBeingTaken = validActions[event];
-          const targetStateLabel = actionBeingTaken && actionBeingTaken.target;
-          if (targetStateLabel) {
-            this.transitionToNewSiblingState(targetStateLabel);
-          }
-        }
+      if (this.state.on[event] && this.state.on[event].target) {
+        this.transition(this.state, event);
       }
     };
     /**
-         * Move machine from current to new state, based on allowed and present action from current state
-         * @param {string} targetStateLabel - name of sibling state to transition to
-         *
-         */
-    Factory.prototype.transitionToNewSiblingState = function (targetStateLabel) {
-      if (!targetStateLabel) return;
-      const parentState = this.getParentOfCurrentGlobalState();
-      const siblingStates = parentState && parentState.states;
-      // check if safe to make transition
-      if (!(siblingStates && siblingStates[targetStateLabel])) return;
+     *
+     * @param {Object} stateToTarget state object to direct Event at
+     * @param {String} event event to trigger on state
+     */
+    Factory.prototype.transition = function (stateToTarget, event) {
+      // this is assuming a SWITCH of figure ONLY, not other changes
+      const nextStateLabel = stateToTarget.on[event].target;
+      const nextState = { ...this.machineDescription.states[nextStateLabel] };
+      if (event.split("-")[0] === "SWITCH") {
+        const currentMajorType = this.state.states.majorPremise.label;
+        const currentMinorType = this.state.states.minorPremise.label;
 
-      this.updateStatePath(targetStateLabel);
+        const majorPremise = nextState.states.majorPremise.states[currentMajorType];
+        const minorPremise = nextState.states.minorPremise.states[currentMinorType];
 
-      if (this.state && this.state.type === "parallel") {
-        if (!this.state.states) return this.disallowedAction("Parallel state has no 'states' property");
-        let parallelStates = Object.keys(this.state.states).join("-");
-        parallelStates = `-${parallelStates}`;
-        console.error("parallel state being entered: ", this.state.label, parallelStates);
-        this.updateStatePath(parallelStates, { parallel: true });
-
-        console.log(this.statePath.indexOf("-"));
-        const firstParallelState = this.statePath.split("-").slice(1);
-        const childStates = firstParallelState;
-        console.log(childStates);
-
-        while (childStates.length) {
-          console.log(this.state.states[childStates[0]]);
-          if (this.state.states[childStates[0]].initial) {
-            // TODO: Need to be able to handle nested parallel states down to nth depth, rather than just one level
-            this.updateStatePath(this.state.states[childStates[0]].initial, { nested: true });
-          }
-
-          console.log(childStates.shift());
-        }
-
-        return;
+        nextState.states = { majorPremise, minorPremise };
       }
 
-      // TODO: If parallel parent, from state path, just append a state (conditionally '^')
-
-      while (this.state.initial) {
-        // default to initial state for this state being entered
-        let nextState = this.state.initial;
-
-        console.log(this.isFirstStateInPath(this.pseudoStates));
-
-        // if next pseudo state valid, take it
-        if (this.pseudoStates && this.pseudoStates[0] && this.state.states[this.pseudoStates[0]]) {
-          console.log("valid pseudo state exists...");
-          nextState = this.pseudoStates.shift();
-        } else {
-          // if pseudo[0] is still valid child somewhere in valid children, keep
-          // else, shift off pseudo[0]
-          if (this.pseudoStates && !this.isFirstStateInPath(this.pseudoStates)) this.pseudoStates.shift();
-        }
-        this.updateStatePath(nextState, { nested: true });
-      }
+      this.state = nextState;
     };
-    /**
-         * Updates the machine's state path, based on various parameters
-         * @param {string} target new state to append/set, depending on config
-         * @param {object} config
-         * @returns
-         */
-    Factory.prototype.updateStatePath = function (target,
-      config = { nested: false, directToAncestor: false, parallel: false }) {
-      const { nested, directToAncestor, parallel } = config;
-      const listOfStates = this.statePath.split(".");
 
-      if (parallel) {
-        return this.statePath += `.${target}`;
-      }
-      if (directToAncestor) {
-        // TODO: Check and store pseudo child states
-        this.pseudoStates = this.getChildPathsRelativeToAncestor(target);
-        console.warn("pseduoL ", this.pseudoStates);
-        return this.statePath = target;
-      }
-      if (!nested) {
-        this.doStateTransitionActions(this.state, "onExit");
-      }
-      if (listOfStates.length === 1 && !nested) {
-        this.statePath = target;
-      } else if (nested) {
-        this.statePath += `.${target}`;
-      } else {
-        listOfStates[listOfStates.length - 1] = target;
-        this.statePath = listOfStates.join(".");
-      }
-      return this.doStateTransitionActions(this.state, "onEntry");
-    };
     return Factory;
   }());
 } catch (e) {
--- syllogism-machine-description.js
+++ syllogism-machine-description.js
@@ -1,78 +1,163 @@
 /* eslint-disable padded-blocks */
 /* eslint-disable no-trailing-spaces */
-const syllogismDescription = {
-  initial: "loading",
-  label: "rootState",
-  states: {
-    loading: {
-      initial: "majorTerm",
-      label: "loading",
-      on: {
-        LOAD: {
-          target: "loaded"
-        },
-        ERROR: {
-          target: "loadingFailed"
-        }
-      },
-      states: {
-        majorTerm: {
-          initial: "subject",
-          label: "majorTerm",
-          states: {
-            subject: {
-              label: "subject"
-            }
-          }
-        },
-        minorTerm: {
-          label: "minorTerm"
-        },
-        conclusion: {
-          label: "conclusion"
-        }
-      }
-    },
-    loaded: {
-      label: "loaded",
-      initial: "clean",
-      on: {
-        ERROR: {
-          target: "error"
-        }
-      },
-      states: {
-        clean: {
-          label: "clean",
-          on: {
-            TOUCH: {
-              target: "dirty"
-            }
-          }
-        },
-        dirty: {
-          label: "dirty",
-          on: {
-            CLEAN: {
-              target: "clean"
-            }
-          }
-        }
-      }
-    },
-    loadingFailed: {
-      label: "loadingFailed",
-      type: "final"
-    },
-    error: {
-      label: "error",
-      type: "final"
-    }
+const figureSwitch = {
+  "SWITCH-FIRST": {
+    target: "first"
   },
-  validStates: [
-    "loaded.clean",
-    "loaded.dirty"
-  ]
+  "SWITCH-SECOND": {
+    target: "second"
+  },
+  "SWITCH-THIRD": {
+    target: "third"
+  },
+  "SWITCH-FOURTH": {
+    target: "fourth"
+  }
+};
+
+const middleTermMajorTerm = {
+  subject: {
+    label: "Middle Term"
+  },
+  predicate: {
+    label: "Major Term"
+  }
+};
+
+const minorTermMiddleTerm = {
+  subject: {
+    label: "Minor Term"
+  },
+  predicate: {
+    label: "Middle Term"
+  }
+};
+
+const majorTermMiddleTerm = {
+  subject: {
+    label: "Major Term"
+  },
+  predicate: {
+    label: "Middle Term"
+  }
+};
+
+const middleTermMinorTerm = {
+  subject: {
+    label: "Middle Term"
+  },
+  predicate: {
+    label: "Minor Term"
+  }
+};
+/**
+ * 
+ * @param {Object} subjectPredicateMajor subject and predicate for major premise
+ * @param {Object} subjectPredicateMinor subject and predicate for minor premise
+ * @returns {Object}
+ */
+const getPremises = function (subjectPredicateMajor, subjectPredicateMinor) {
+  return {
+    majorPremise: {
+      initial: "A",
+      states: {
+        A: {
+          states: { ...subjectPredicateMajor }
+        },
+        E: {
+          states: { ...subjectPredicateMajor }
+        },
+        I: {
+          states: { ...subjectPredicateMajor }
+        },
+        O: {
+          states: { ...subjectPredicateMajor }
+        }
+      }
+    },
+    minorPremise: {
+      initial: "A",
+      states: {
+        A: {
+          states: { ...subjectPredicateMinor }
+        },
+        E: {
+          states: { ...subjectPredicateMinor }
+        },
+        I: {
+          states: { ...subjectPredicateMinor }
+        },
+        O: {
+          states: { ...subjectPredicateMinor }
+        }
+      }
+    }
+  };
+};
+
+const syllogismDescription = {
+  initial: "first",
+  states: {
+    first: {
+      on: {
+        "SWITCH-MAJOR": {
+          target: "second"
+        },
+        "SWITCH-MINOR": {
+          target: "third"
+        },
+        ...figureSwitch
+      },
+      states: {
+        ...getPremises(middleTermMajorTerm, minorTermMiddleTerm)
+      }
+    }, 
+    second: {
+      on: {
+        "SWITCH-MAJOR": {
+          target: "first"
+        },
+        "SWITCH-MINOR": {
+          target: "fourth"
+        },
+        ...figureSwitch
+      },
+      states: {
+        ...getPremises(majorTermMiddleTerm, minorTermMiddleTerm)
+      }
+
+    },
+    third: {
+      on: {
+        "SWITCH-MAJOR": {
+          target: "fourth"
+        },
+        "SWITCH-MINOR": {
+          target: "first"
+        },
+        ...figureSwitch
+      },
+      states: {
+        ...getPremises(middleTermMajorTerm, middleTermMinorTerm)
+      }
+
+    }, 
+    fourth: {
+      on: {
+        "SWITCH-MAJOR": {
+          target: "third"
+        },
+        "SWITCH-MINOR": {
+          target: "second"
+        },
+        ...figureSwitch
+      },
+      states: {
+        ...getPremises(majorTermMiddleTerm, middleTermMinorTerm)
+      }
+    }
+  }
+ 
 };
 
 window.syllogismDescription = syllogismDescription;
