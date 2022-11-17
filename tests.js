79cad94dcbb078317b1019e2976475fe40740b32
John Jordan <johnjordan1985@gmail.com>
2022-11-16 19:20:28 -0800

    refactor: pass nested function as cb to event listener executor

--- tests.js
+++ tests.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-cond-assign */
-const check = (elemString) => {
+const check = (elemString) => () => {
   const elems = document.querySelectorAll(elemString);
   for (let i = 0, e; e = elems[i]; i++) {
     if (!e.id) console.warn("e has no ID: ", e);
   }
 };
 document.addEventListener("change", check("input, select"));
-document.addEventListener("change", check("a, button"));
+
+document.addEventListener("click", check("a, button, [data-event]"));
